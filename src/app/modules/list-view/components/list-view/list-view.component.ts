4e81c4022dbec7aefdd3869eae6a6c9c9214484c
Alexander <occvlt@yandex.ru>
2019-10-18 11:40:03 +0400

    add list view component for main and favs page

--- src/app/modules/list-view/components/list-view/list-view.component.ts
+++ src/app/modules/list-view/components/list-view/list-view.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { YtItem } from '../../../../interfaces/yt-response';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-list-view',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-view.component.scss']
 })
 export class ListViewComponent implements OnInit {
+  @Input() items: YtItem[];
+  @Input() pageToken: string;
+  @Input() showLoadMoreButton = false;
 
-  constructor() { }
+  @Output() loadMoreClicked = new EventEmitter<string>();
+  @Output() addToFavsClicked = new EventEmitter<YtItem>();
+  @Output() removeFromFavsClicked = new EventEmitter<YtItem>();
+
+  searchForm = this.fb.group({
+    search: ''
+  });
+
+  private triggerPipe;
+
+  constructor(private readonly fb: FormBuilder) { }
 
   ngOnInit() {
   }
 
+  loadMore(pageToken: string) {
+    this.loadMoreClicked.emit(pageToken);
+  }
+
+  addToFavs(item: YtItem) {
+    this.addToFavsClicked.emit(item);
+    this.updatePipe();
+  }
+
+  removeFromFavs(item: YtItem) {
+    this.removeFromFavsClicked.emit(item);
+    this.updatePipe();
+  }
+
+  trackByFn(index: number, item: YtItem): string {
+    return item.id;
+  }
+
+  updatePipe() {
+    this.triggerPipe = new Date();
+  }
 }
--- src/app/modules/list-view/list-view.module.ts
+++ src/app/modules/list-view/list-view.module.ts
@@ -1,17 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatCardModule, MatDividerModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDividerModule,
+  MatIconModule,
+  MatInputModule,
+  MatProgressSpinnerModule, MatTooltipModule
+} from '@angular/material';
 
 import { ListViewComponent } from './components/list-view/list-view.component';
+import { VerifyInFavsPipe } from '../../pipes/verify-in-favs.pipe';
+import { FilterPipe } from '../../pipes/filter.pipe';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [ListViewComponent],
+  declarations: [ListViewComponent, VerifyInFavsPipe, FilterPipe],
   imports: [
     CommonModule,
     MatCardModule,
+    MatIconModule,
     MatDividerModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatProgressSpinnerModule,
+    ReactiveFormsModule
   ],
   exports: [ ListViewComponent ]
 })
